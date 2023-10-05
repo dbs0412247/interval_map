b08aebed109b49be6eb902e821d9f981eda971c6
Adrian Yuen <adriany@searidgetech.com>
2023-10-05 16:23:17 +0100

    Working commit with bugs

---
+++ main.cpp
@@ -0,0 +1,251 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <map>
+
+/**
+ * @brief
+ *
+ * @tparam K
+ * Key type K
+    - besides being copyable and assignable, is less-than comparable via operator<, and
+    - does not implement any other operations, in particular no equality comparison or arithmetic operators.
+ * @tparam V
+ * Value type V
+    - besides being copyable and assignable, is equality-comparable via operator==, and
+    - does not implement any other operations.
+ */
+
+template<typename K, typename V>
+class interval_map {
+    //friend void IntervalMapTest();
+    V m_valBegin;
+    std::map<K,V> m_map;
+public:
+    // constructor associates whole range of K with val
+    interval_map(V const& val)
+    : m_valBegin(val)
+    {}
+
+    // Assign value val to interval [keyBegin, keyEnd).
+    // Overwrite previous values in this interval.
+    // Conforming to the C++ Standard Library conventions, the interval
+    // includes keyBegin, but excludes keyEnd.
+    // If !( keyBegin < keyEnd ), this designates an empty interval,
+    // and assign must do nothing.
+    void assign( K const& keyBegin, K const& keyEnd, V const& val ) {
+
+        // Empty interval case
+        if ( !( keyBegin < keyEnd ) ) {
+            return;
+        }
+
+        bool isValEqValBegin = (val == m_valBegin);
+
+        // In theory, the number of entries in m_map should be an non-negative integer N, with N != 1 (assuming no +ve infinity K).
+        // i.e. N = 0, 2, 3, 4, ...
+
+        // N == 0 case
+        if ( m_map.empty() ) {
+
+            // first internal in m_map
+            if ( !isValEqValBegin ) {
+                // insert the beginning and end m_map entries
+                m_map[keyBegin] = val;
+                m_map[keyEnd] = m_valBegin;
+            }
+            // Nothing needs to be done for the else case:
+            // i.e. if the m_map is empty and the range assigned has equal value to m_valBegin.
+
+            return;
+        }
+
+        V valEnd = this->operator[](keyEnd);
+        auto itBegin = m_map.upper_bound(keyBegin);
+
+        if (itBegin == m_map.begin()) {
+            if  (!isValEqValBegin) {
+                auto res = m_map.insert_or_assign(keyBegin, val);
+                itBegin = res.first;
+            }
+        }
+        else if (itBegin == m_map.end()) {
+
+            if ((std::prev(itBegin))->second == val) {
+                // itBegin is the largest element, and its value is already correct.
+                return;
+            }
+
+            V valTrailing = (std::prev(itBegin))->second;
+            m_map[keyBegin] = val;
+            m_map[keyEnd] = valTrailing;
+            return;
+        }
+        else {  // itBegin is somewhere in the middle of m_map
+
+            if ((std::prev(itBegin))->second == val) {
+                itBegin--;
+            }
+            else {
+                auto res = m_map.insert_or_assign(keyBegin, val);
+                itBegin = res.first;
+            }
+        }
+
+        // Now figure out where itEnd is.
+        auto itEnd = m_map.upper_bound(keyEnd);
+
+        if (itEnd == m_map.begin()) // both itBegin and itEnd are before the first element
+        {
+            if  (!isValEqValBegin) {
+                m_map.insert_or_assign(keyEnd, m_valBegin);
+            }
+            return;
+        }
+
+        // Remove all entries with key K in m_map where keyBegin < K < keyEnd.
+        for (auto it = std::next(itBegin); it != itEnd;)
+        {
+            it = m_map.erase(it);
+        }
+
+        if (val != valEnd)
+        {
+            m_map.insert_or_assign(keyEnd, valEnd);
+        }
+    }
+
+    // look-up of the value associated with key
+    V const& operator[]( K const& key ) const {
+        auto it=m_map.upper_bound(key);
+        if(it==m_map.begin()) {
+            return m_valBegin;
+        } else {
+            return (--it)->second;
+        }
+    }
+
+    void print() const {
+        for (const auto &node : m_map) {
+            std::cout << "\t[ " << node.first << " ] => \'" << node.second << "\'" << std::endl;
+        }
+    }
+
+    std::size_t GetSize() const {
+        return m_map.size();
+    }
+
+    bool isCanonical() const {
+        V lastVal = m_valBegin;
+        for (auto it = m_map.begin(); it != m_map.end(); it++)
+        {
+            if (it->second == lastVal)
+                return false;
+
+            lastVal = it->second;
+        }
+        return true;
+    }
+};
+
+// Many solutions we receive are incorrect. Consider using a randomized test
+// to discover the cases that your implementation does not handle correctly.
+// We recommend to implement a test function that tests the functionality of
+// the interval_map, for example using a map of int intervals to char.
+
+bool RunTest(int keyBegin, int keyEnd, char val)
+{
+    char outputExpected[9];
+
+    interval_map<int, char> test_map('A');
+    for (int i = 0; i < 9; i++)
+    {
+        outputExpected[i] = 'A';
+    }
+
+    test_map.assign(3, 5, 'B');
+    for (int i = 3; i < 5; i++)
+    {
+        outputExpected[i] = 'B';
+    }
+
+    test_map.assign(keyBegin, keyEnd, val);
+    for (int i = keyBegin; i < keyEnd; i++)
+    {
+        outputExpected[i] = val;
+    };
+
+    bool isFailed = false;
+    for (int i = 0; i < 9; i++)
+    {
+        if (outputExpected[i] != test_map[i])
+        {
+            isFailed = true;
+            break;
+        }
+    }
+
+    if (isFailed)
+    {
+        std::cout << "RunTest(" << keyBegin << "," << keyEnd << ",\'" << val << "\') found mismatched output:" << std::endl;
+        for (int i = 0; i < 9; i++)
+        {
+            std::cout << i;
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 9; i++)
+        {
+            std::cout << outputExpected[i];
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < 9; i++)
+        {
+            std::cout << test_map[i];
+        }
+        std::cout << std::endl;
+
+        test_map.print();
+
+        std::cout << std::endl;
+    }
+
+    return test_map.isCanonical();
+}
+
+void BruteForceTest()
+{
+    std::vector<std::string> vecNotCanonical;
+
+    for (int i = 1; i <= 7; i++)
+    {
+        for (int j = 1; j <= 7; j++)
+        {
+            for (char c = 'A'; c <= 'C'; c++)
+            {
+                if (!RunTest(i, j, c))
+                {
+                    vecNotCanonical.push_back("RunTest(" + std::to_string(i) + "," + std::to_string(j) + ",\'" + c + "\') is not canonical");
+                }
+            }
+        }
+    }
+
+    if (vecNotCanonical.empty())
+    {
+        std::cout << "All canonical!" << std::endl;
+    }
+    else
+    {
+        for (auto it = vecNotCanonical.begin(); it != vecNotCanonical.end(); it++)
+        {
+            std::cout << *it << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    BruteForceTest();
+    //RunTest(1,4,'A');
+}
